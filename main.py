2b83386fdb82bcca558cdf27ecf3c592d7602a43
Anonymous <ilvi142014@gmail.com>
2022-12-25 01:45:56 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import iluxaMod as ilm
 
+database = ilm.postgreSQL_connect(user="postgres", 
+    password="armageddon", 
+    database="powersport", 
+    host="illyashost.ddns.net")
+
+sql = database.sql
+db = database.db
+
+
