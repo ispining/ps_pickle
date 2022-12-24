9cec9bac6d6062b6cf58fd319d10440bd543fcb7
Anonymous <ilvi142014@gmail.com>
2022-12-25 01:54:59 +0200

    update main.py {remove config content}

--- main.py
+++ main.py
@@ -1,22 +1,2 @@
-import os
+from config import *
 
-import iluxaMod as ilm
-
-database = ilm.postgreSQL_connect(user="postgres", 
-    password="armageddon", 
-    database="powersport", 
-    host="illyashost.ddns.net")
-
-sql = database.sql
-db = database.db
-
-ILMtools = ilm.tools()
-pick = ILMtools.pick
-unpick = ILMtools.unpick
-
-FILESLIST = []
-
-for filename in FILESLIST:
-    pass
-    
-    
